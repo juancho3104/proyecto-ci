a871f99a7e97cc2a8ad09ad860f332e46b8eabf3
juan felipe <jufeli0104@gmail.com>
2025-03-05 16:22:18 +0000

    segundo commit

--- test_funciones.py
+++ test_funciones.py
@@ -2,6 +2,6 @@ from proyecto import es_primo
 
 def test_es_primo():
     assert es_primo(5) is True
-    assert es_primo(10) is True
+    assert es_primo(3) is True
     assert es_primo(2) is True
     assert es_primo(4) is False
